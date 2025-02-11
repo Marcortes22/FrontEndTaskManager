1a19f322b6d1e26bb9c12ae104eb7f6ec7d9d0c8
Marco Cortes <marcortes.stiven@gmail.com>
2025-02-10 19:17:44 -0600

    add relative path to Assets

---
+++ src/Constants/BackGroundImages.ts
@@ -0,0 +1,98 @@
+import thumbnail_1 from '@Assets/thumbnails/city-thumbnail.webp';
+import background_1 from '@Assets/backgroundImages/city-background.webp';
+
+import thumbnail_2 from '@Assets/thumbnails/dark-forest-thumbnail.webp';
+import background_2 from '@Assets/backgroundImages/dark-forest-background.webp';
+
+import thumbnail_3 from '@Assets/thumbnails/forest-thumbnail.webp';
+import background_3 from '@Assets/backgroundImages/forest-background.webp';
+
+import thumbnail_4 from '@Assets/thumbnails/glasses-thumbnail.webp';
+import background_4 from '@Assets/backgroundImages/glasses-background.webp';
+
+import thumbnail_5 from '@Assets/thumbnails/hill-thumbnail.webp';
+import background_5 from '@Assets/backgroundImages/hill-background.webp';
+
+import thumbnail_6 from '@Assets/thumbnails/sunset-thumbnail.webp';
+import background_6 from '@Assets/backgroundImages/sunset-background.webp';
+
+import thumbnail_7 from '@Assets/thumbnails/mountain-thumbnail.webp';
+import background_7 from '@Assets/backgroundImages/mountain-background.webp';
+
+import thumbnail_8 from '@/assets/thumbnails/rock-thumbnail.webp';
+import background_8 from '@/assets/backgroundImages/rock-background.webp';
+
+import thumbnail_9 from '@/assets/thumbnails/sea-thumbnail.webp';
+import background_9 from '@/assets/backgroundImages/sea-background.webp';
+
+import thumbnail_10 from '@/assets/thumbnails/sea-star-thumbnail.webp';
+import background_10 from '@/assets/backgroundImages/sea-star-background.webp';
+
+import thumbnail_11 from '@/assets/thumbnails/snow-mountain-thumbnail.webp';
+import background_11 from '@/assets/backgroundImages/snow-mountain-background.webp';
+
+import thumbnail_12 from '@/assets/thumbnails/stars-thumbnail.webp';
+import background_12 from '@/assets/backgroundImages/stars-background.webp';
+
+export const backgroundImagesItems = [
+  {
+    thumbnail: thumbnail_1,
+    img: background_1,
+    title: 'city background',
+  },
+  {
+    thumbnail: thumbnail_2,
+    img: background_2,
+    title: 'Dark forest background',
+  },
+  {
+    thumbnail: thumbnail_3,
+    img: background_3,
+    title: 'Forest background',
+  },
+  {
+    thumbnail: thumbnail_4,
+    img: background_4,
+    title: 'Glasses background',
+  },
+  {
+    thumbnail: thumbnail_5,
+    img: background_5,
+    title: 'Hill background',
+  },
+  {
+    thumbnail: thumbnail_6,
+    img: background_6,
+    title: 'Mars background',
+  },
+  {
+    thumbnail: thumbnail_7,
+    img: background_7,
+    title: 'Mountain background',
+  },
+  {
+    thumbnail: thumbnail_8,
+    img: background_8,
+    title: 'Rock background',
+  },
+  {
+    thumbnail: thumbnail_9,
+    img: background_9,
+    title: 'Sea background',
+  },
+  {
+    thumbnail: thumbnail_10,
+    img: background_10,
+    title: 'Star background',
+  },
+  {
+    thumbnail: thumbnail_11,
+    img: background_11,
+    title: 'Snow mountain background',
+  },
+  {
+    thumbnail: thumbnail_12,
+    img: background_12,
+    title: 'Stars background',
+  },
+];
